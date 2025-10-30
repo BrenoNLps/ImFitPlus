3a9e19e045e4cf16dc242e6aba2fd761d9995c10
BrenoNLps <brenolopes.xy@gmail.com>
2025-10-29 22:39:19 -0300

    feat: Adiciona activity DadosPessoais

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3011704/imfitplus/DadosPessoais.kt
@@ -0,0 +1,20 @@
+package br.edu.ifsp.scl.ads.prdm.sc3011704.imfitplus
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DadosPessoais : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dados_pessoais)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
