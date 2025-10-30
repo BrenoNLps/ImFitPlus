076e53d086c2d00c76b7f9ba9e9ba52a300758b7
BrenoNLps <brenolopes.xy@gmail.com>
2025-10-30 01:15:31 -0300

    refactor: Alteracao de view para ViewBinding

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3011704/imfitplus/DadosPessoais.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3011704/imfitplus/DadosPessoais.kt
@@ -1,20 +1,21 @@
 package br.edu.ifsp.scl.ads.prdm.sc3011704.imfitplus
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc3011704.imfitplus.databinding.ActivityDadosPessoaisBinding
 
 class DadosPessoais : AppCompatActivity() {
+    private lateinit var binding: ActivityDadosPessoaisBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_dados_pessoais)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding= ActivityDadosPessoaisBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
