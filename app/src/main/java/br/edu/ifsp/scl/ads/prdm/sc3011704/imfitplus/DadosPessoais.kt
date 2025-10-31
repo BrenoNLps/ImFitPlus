b2b99c84eca43d512b6c8796050fb3ae5873a1ec
BrenoNLps <brenolopes.xy@gmail.com>
2025-10-31 05:54:50 -0300

    feat: Adiciona validacao e envio de dados por intent para a activity resultado

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3011704/imfitplus/DadosPessoais.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3011704/imfitplus/DadosPessoais.kt
@@ -1,11 +1,9 @@
 package br.edu.ifsp.scl.ads.prdm.sc3011704.imfitplus
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.scl.ads.prdm.sc3011704.imfitplus.databinding.ActivityDadosPessoaisBinding
 
 class DadosPessoais : AppCompatActivity() {
@@ -16,6 +14,43 @@ class DadosPessoais : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityDadosPessoaisBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.enviarBt.setOnClickListener {
+            val nome         = binding.nomeEt.text.toString()
+            val idadeString  = binding.idadeEt.text.toString()
+            val alturaString = binding.alturaEt.text.toString()
+            val pesoString   = binding.pesoEt.text.toString()
+
+
+            if (nome.isBlank()   || idadeString.isBlank()  || alturaString.isBlank() || pesoString.isBlank()){
+                Toast.makeText(
+                    this,"Preencha todos os campos!",
+                    Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val idade  = idadeString.toInt()
+            val altura = alturaString.toDouble()
+            val peso   = pesoString.toDouble()
+            val sexo = when (binding.sexoRg.checkedRadioButtonId){
+                R.id.masculino_rb -> "masculino"
+                R.id.feminino_rb  -> "feminino"
+                else -> ""
+            }
+            val atividade = binding.nivelAtividadeSp.selectedItem.toString()
+            val imc = peso/ (altura*altura)
+
+            val resultado = Intent(this, ResultadoIMC::class.java)
+            intent.putExtra("nome"     ,nome)
+            intent.putExtra("imc"      ,imc)
+            intent.putExtra("sexo"     ,sexo)
+            intent.putExtra("idade"    ,idade)
+            intent.putExtra("peso"     ,peso)
+            intent.putExtra("altura"   ,altura)
+            intent.putExtra("atividade",atividade)
+            startActivity(resultado)
+        }
 
     }
 }
