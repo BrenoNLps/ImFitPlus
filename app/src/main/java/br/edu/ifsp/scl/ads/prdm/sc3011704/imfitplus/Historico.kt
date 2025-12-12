91cf59dd8fd853819885961fc8e5f68d9149860b
brenonlps <brenolopes.xy@gmail.com>
2025-12-12 05:08:24 -0300

    feat: Exibe histórico de usuários na tela de Histórico

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3011704/imfitplus/Historico.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3011704/imfitplus/Historico.kt
@@ -2,7 +2,12 @@ package br.edu.ifsp.scl.ads.prdm.sc3011704.imfitplus
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.edu.ifsp.scl.ads.prdm.sc3011704.imfitplus.databinding.ActivityHistoricoBinding
+import br.edu.ifsp.scl.ads.prdm.sc3011704.imfitplus.usuario.adapter.UsuarioAdapter
+import br.edu.ifsp.scl.ads.prdm.sc3011704.imfitplus.usuario.data.DatabaseBuilder
+import kotlinx.coroutines.launch
 
 class Historico : AppCompatActivity() {
     private lateinit var binding: ActivityHistoricoBinding
@@ -12,6 +17,13 @@ class Historico : AppCompatActivity() {
         binding= ActivityHistoricoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val db = DatabaseBuilder.getInstance(this)
+
+        lifecycleScope.launch {
+            val listaUsuarios = db.usuarioDao().historicoCompleto()
+            binding.historicoRv.layoutManager = LinearLayoutManager(this@Historico)
+            binding.historicoRv.adapter = UsuarioAdapter(listaUsuarios)
+        }
 
 
     }
