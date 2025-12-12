bf11c2729f157f7db422512a7516c90d6dee43bf
brenonlps <brenolopes.xy@gmail.com>
2025-12-12 03:56:28 -0300

    feat: Adiciona DatabaseBuilder para instanciar o banco de dados

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3011704/imfitplus/data/DatabaseBuilder.kt
@@ -0,0 +1,19 @@
+package br.edu.ifsp.scl.ads.prdm.sc3011704.imfitplus.data
+
+import android.content.Context
+import androidx.room.Room
+
+object DatabaseBuilder {
+    private var INSTANCE: AppDatabase? = null
+
+    fun getInstance(context: Context): AppDatabase {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "imfitplus_db"
+            ).build()
+        }
+        return INSTANCE!!
+    }
+}
