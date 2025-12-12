48eceabd88a9e7eaa8916a39448332a0eaa3d8f0
brenonlps <brenolopes.xy@gmail.com>
2025-12-12 06:00:16 -0300

    feat: Preenche dados do usuário ao carregar da tela de histórico

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3011704/imfitplus/DadosPessoais.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3011704/imfitplus/DadosPessoais.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import br.edu.ifsp.scl.ads.prdm.sc3011704.imfitplus.databinding.ActivityDadosPessoaisBinding
+import br.edu.ifsp.scl.ads.prdm.sc3011704.imfitplus.usuario.data.DatabaseBuilder
 import br.edu.ifsp.scl.ads.prdm.sc3011704.imfitplus.usuario.model.Usuario
+import kotlinx.coroutines.launch
 
 class DadosPessoais : AppCompatActivity() {
     //tela 2
@@ -16,6 +19,38 @@ class DadosPessoais : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityDadosPessoaisBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val db = DatabaseBuilder.getInstance(this)
+
+        // Recupera o usuário selecionado no histórico
+        val usuarioId = intent.getIntExtra("usuario_id", -1)
+        if (usuarioId != -1) {
+            lifecycleScope.launch {
+                val usuario = db.usuarioDao().getUsuarioById(usuarioId)
+                if (usuario != null) {
+                    binding.nomeEt.setText(usuario.nome)
+                    binding.idadeEt.setText(usuario.idade.toString())
+
+                    if (usuario.sexo == "masculino") {
+                        binding.sexoRg.check(R.id.masculino_rb)
+                    } else if (usuario.sexo == "feminino") {
+                        binding.sexoRg.check(R.id.feminino_rb)
+                    }
+
+                    binding.alturaEt.setText(usuario.altura.toString())
+                    binding.pesoEt.setText(usuario.peso.toString())
+
+                    // Seleciona a opção correta do Spinner comparando texto
+                    val adapter = binding.nivelAtividadeSp.adapter
+                    for (i in 0 until adapter.count) {
+                        if (adapter.getItem(i).toString() == usuario.atividade) {
+                            binding.nivelAtividadeSp.setSelection(i)
+                            break
+                        }
+                    }
+                }
+            }
+        }
 
 
         binding.enviarBt.setOnClickListener {
